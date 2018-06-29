04f4036a4155166bb1541a3669e548c5996b6f56
Przemek Bednarczyk <pjbednarczyk@gmail.com>
2018-06-29 15:58:25 +0200

    add newsletter

--- out.js
+++ out.js
@@ -23342,11 +23342,50 @@ var Footer = function (_React$Component) {
                 { className: "footerContainer" },
                 _react2.default.createElement(
                     "div",
-                    null,
+                    { className: "newsletterBox container" },
                     _react2.default.createElement(
-                        "h6",
+                        "li",
                         null,
-                        "Newsletter"
+                        _react2.default.createElement(
+                            "h6",
+                            null,
+                            _react2.default.createElement(
+                                "strong",
+                                null,
+                                "Newsletter"
+                            ),
+                            " Sign Up"
+                        ),
+                        _react2.default.createElement(
+                            "p",
+                            null,
+                            "(Get 30% OFF coupon today subscribers)"
+                        )
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement(
+                            "p",
+                            null,
+                            "Join ",
+                            _react2.default.createElement(
+                                "strong",
+                                null,
+                                "35.000+ subscribers"
+                            ),
+                            " and get a new discount coupon on every Wednesday."
+                        )
+                    ),
+                    _react2.default.createElement(
+                        "li",
+                        null,
+                        _react2.default.createElement("input", { placeholder: "Your email address..." }),
+                        _react2.default.createElement(
+                            "span",
+                            null,
+                            "SUBSCRIBE"
+                        )
                     )
                 )
             );
--- src/Footer/index.js
+++ src/Footer/index.js
@@ -4,10 +4,27 @@ class Footer extends React.Component {
     render() {
         return (
             <div className="footerContainer">
-                <div>
-<h6>Newsletter</h6>
+                <div className="newsletterBox container">
+                    <li>
+                        <h6>
+                            <strong>Newsletter</strong> Sign Up
+                        </h6>
+                        <p>
+                            (Get 30% OFF coupon today subscribers)
+                        </p>
+                    </li>
+                    <li>
+                        <p>
+                            Join <strong>35.000+ subscribers</strong> and get a new discount coupon on every Wednesday.
+                        </p>
+                    </li>
+                    <li>
+                        <input placeholder="Your email address..."/>
+                        <span>SUBSCRIBE</span>
+                    </li>
                 </div>
             </div>
+
         );
     }
 }
